90a30c6a7e76c569ab1b02f4e899281cc601d2a6
Mike James <michael.dacampora54@spsmail.cuny.edu>
2017-08-31 22:44:23 -0400

    cleaned up

--- ShroomsScript.R
+++ ShroomsScript.R
@@ -5,56 +5,44 @@ colnames(shrooms) <- c("Edible", "CapShape", "CapSurface", "CapColor", "Bruises"
                        "GillSize", "GillColor", "StalkShape", "StalkRoot", "StalkSurfaceAboveRing",
                        "StalkSurfaceBelowRing", "StalkColorAboveRing", "StalkColorBelowRing", "VeilType",
                        "VeilColor", "RingNumber", "RingType", "SporePrintColor", "Population", "Habitat")
-str(shrooms)
-head(shrooms)
 
-# change values in column
-#shrooms$Edible <-  as.character(shrooms$Edible)
-#shrooms$Edible[shrooms$Edible == "e"] <- "edible"
-#shrooms$Edible[shrooms$Edible == "p"] <- "poisonous"
 
-#reassigned items to legible informationdplyr
-shrooms$Edible <- shrooms$Edible %>% recode("e"="edible", "p"="poinsonous")
-shrooms$CapShape <- shrooms$CapShape %>% recode("b"="bell", "c"="conical", "f"="flat",
-                                                "k"="knobbed", "s"="sunken","x"="convex")
-shrooms$CapSurface <- shrooms$CapSurface %>% recode()
-shrooms$CapColor <- shrooms$CapColor %>% recode()
-shrooms$Bruises <- shrooms$Bruises %>% recode()
-shrooms$Odor <- shrooms$Odor %>% recode()
-shrooms$GillAtt <- shrooms$GillAtt %>% recode()
-shrooms$GillSpacing <- shrooms$GillSpacing %>% recode()
-shrooms$GillSize <- shrooms$GillSize %>% recode()
-shrooms$GillColor <- shrooms$GillColor %>% recode()
-shrooms$StalkShape <- shrooms$StalkShape %>% recode()
-shrooms$StalkRoot <- shrooms$StalkRoot %>% recode()
-shrooms$StalkSurfaceBelowRing <- shrooms$StalkSurfaceBelowRing %>% recode()
-shrooms$StalkSurfaceAboveRing <- shrooms$StalkSurfaceAboveRing %>% recode()
-shrooms$StalkColorBelowRing <- shrooms$StalkColorBelowRing %>% recode()
-shrooms$StalkColorAboveRing <- shrooms$StalkColorAboveRing %>% recode()
-shrooms$VeilType <- shrooms$VeilType %>% recode()
-shrooms$VeilColor <- shrooms$VeilColor %>% recode()
-shrooms$RingNumber <- shrooms$RingNumber %>% recode()
-shrooms$RingType <- shrooms$RingType %>% recode()
-shrooms$SporePrintColor <- shrooms$SporePrintColor %>% recode()
-shrooms$Population <- shrooms$Population %>% recode()
-shrooms$Habitat <- shrooms$Habitat %>% recode()
+#Change values in column,reassigned items to legible information with dplyr
+shrooms$Edible <- shrooms$Edible %>% 
+  recode("e"="edible", "p"="poinsonous")
 
-#$ Bruises                 : Factor w/ 2 levels 
-#$ Odor                    : Factor w/ 9 levels 
-#$ Gill Att                : Factor w/ 2 levels 
-#$ Gill Spacing            : Factor w/ 2 levels 
-#$ Gill Size               : Factor w/ 2 levels 
-#$ Gill Color              : Factor w/ 12 levels 
-#$ Stalk Shape             : Factor w/ 2 levels 
-#$ Stalk Root              : Factor w/ 5 levels 
-#$ Stalk Surface Above Ring: Factor w/ 4 levels 
-#$ Stalk Surface Below Ring: Factor w/ 4 levels 
-#$ Stalk Color Above Ring  : Factor w/ 9 levels 
-#$ Stalk Color Below Ring  : Factor w/ 9 levels 
-#$ Veil Type               : Factor w/ 1 level 
-#$ Veil Color              : Factor w/ 4 levels 
-#$ Ring Number             : Factor w/ 3 levels 
-#$ Ring Type               : Factor w/ 5 levels 
-#$ Spore Print Color       : Factor w/ 9 levels 
-#$ Population              : Factor w/ 6 levels 
-#$ Habitat                 : Factor w/ 7 levels 
+shrooms$CapShape <- shrooms$CapShape %>% 
+  recode("b"="bell", 
+         "c"="conical", 
+         "f"="flat",
+         "k"="knobbed", 
+         "s"="sunken",
+         "x"="convex")
+
+shrooms$CapSurface <- shrooms$CapSurface %>% 
+  recode('f'='fibrous',
+         'g'='grooves',
+         'y'='scaly',
+         's'='smooth')
+
+shrooms$Odor <- shrooms$Odor %>% 
+  recode('a'='almond',
+         'l'='anise',
+         'c'='creosote',
+         'y'='fishy',
+         'f'='foul',
+         'm'='musty',
+         'n'='none',
+         'p'='pungent',
+         's'='spicy')
+
+shrooms$Population <- shrooms$Population %>% 
+  recode('a'='abundant',
+         'c'='clustered',
+         'n'='numerous',
+         's'='scattered',
+         'v'='several',
+         'y'='solitary')
+
+test_set <- shrooms %>% select(Edible, CapShape, CapSurface, Odor, Population)
+head(test_set)
